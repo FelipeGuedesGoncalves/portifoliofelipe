1003339f6284d8476633ac7646503ec557d18f38
FelipeGuedesGoncalves <felipeguedes6559@gmail.com>
2025-11-04 21:06:35 -0300

    botão de download

--- src/components/buttons.tsx
+++ src/components/buttons.tsx
@@ -9,7 +9,7 @@ export interface ButtonSpecs{
 
 export function NavButton({name, level}: ButtonSpecs){
     return(
-        <button className="bg-transparent bg-none h-10 p-2 text-xl mx-10"
+        <button className="bg-transparent bg-none h-10 p-2 text-xl mx-10 hover:underline"
         onClick={() => window.scrollTo({ top: level, behavior: "smooth" })}>
             {name}
         </button>
@@ -18,7 +18,7 @@ export function NavButton({name, level}: ButtonSpecs){
 
 export function DownloadBtn(){
     return(
-        <button className="text-xl flex text-black bg-white mr-10 p-2 px-4 mt-2">
+        <button className="text-xl flex text-black bg-white mr-10 p-2 px-4 mt-2    hover:bg-transparent hover:text-white  transition ease-in-out duration-200">
             Download CV
             <Download className="ml-5"/>
         </button>
