55ff330776882b6b27193284a1836c86531da55c
FelipeGuedesGoncalves <felipeguedes6559@gmail.com>
2025-11-06 17:16:17 -0300

    "Sobre mim" estilizado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
           </section>
         </nav>
         <div className=" w-250 flex items-center justify-evenly mt-25">
-          <Image className="" alt="" src={"/Felipe.png"} height={300} width={300}></Image>
+          <Image className="mr-10" alt="" src={"/Felipe.png"} height={260} width={260}></Image>
           <div className="h-40 flex flex-col justify-between ">
             <h1 className={`${S4.className} text-white text-2xl w-150`}>Prazer, me chamo Felipe Guedes Gonçalves</h1>
             <p className={`${STMono.className} text-white`}>E sou um desenvolvedor web e mobile</p>
@@ -39,6 +39,36 @@ export default function Home() {
           </div>
         </div>
       </header>
+
+      <section className="w-full h-170 bg-neutral-800 p-20 flex flex-col items-center justify-center">
+        <h2 className={`${S4.className} text-white text-xl w-280`}>Sobre Mim</h2>
+        <div className="mt-10 flex flex-row justify-between text-justify w-280">
+
+          <p className={`${STMono.className} text-white w-[60%]`}>Olá, meu nome é Felipe, e sou apaixonado por design e pela criação de experiências únicas. Desde que ingressei no mundo da tecnologia, fico cada vez mais fascinado com o impacto positivo que podemos gerar por meio de soluções inovadoras. Acredito que a tecnologia deve sempre estar a serviço da melhoria contínua da humanidade.
+            <br />
+            <br />
+            Durante minha trajetória na Faculdade de Informática e Administração Paulista (FIAP), tive a oportunidade de adquirir uma ampla gama de conhecimentos, como Programação Orientada a Objetos (POB), DevOps, responsividade, garantia de qualidade, trabalho em equipe e gestão de prazos.
+            <br />
+            <br />
+            No entanto, o que realmente despertou minha paixão foi o desenvolvimento front-end, tanto para web quanto para mobile. Se você está buscando alguém com experiência e interesse em Next.js, React, HTML, CSS, JavaScript, TypeScript, React Native e Kotlin, não hesite em acessar a seção de contato abaixo.
+          </p>
+
+          <div className="w-[35%] h-full">
+            <div>
+              <h4 className={`${S4.className} text-white text-sm w-full`}>Minha Formação Acadêmica</h4>
+              <p className={`${STMono.className} text-yellow-400 mt-5`}>Tecnólogo em Análise e desenvolvimento de sistemas</p>
+              <p className={`${STMono.className} text-white mt-3`}>FIAP - São Paulo - SP, Brasil | fev 2023 - nov 2024</p>
+            </div>
+            <div>
+              <h4 className={`${S4.className} mt-10 text-white text-sm w-full`}>Línguas faladas</h4>
+              <p className={`${STMono.className} text-white mt-5`}><span className="text-yellow-400">Português:</span> Língua Nativa</p>
+              <p className={`${STMono.className} text-white mt-3`}><span className="text-yellow-400">Inglês:</span> Fluente (nível C1) - Alta proficiência em leitura e escuta, aprimorando fala e escrita</p>
+            </div>
+
+          </div>
+        </div>
+      </section>
+      
     </main>
   );
 }
