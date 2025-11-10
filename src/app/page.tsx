9bc7b858561f08c2aa493d1785436b5f3f7726d5
FelipeGuedesGoncalves <felipeguedes6559@gmail.com>
2025-11-10 16:27:05 -0300

    mais breakpoints, correção na foto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ const S4 = Sixtyfour({
 export default function Home() {
   return (
     <main>
-      <header className="xl:bg-green-500 lg:bg-yellow-700 md:bg-orange-400 sm:bg-red-500 shadow-[inset_0px_-140px_180px_60px_rgb(224_224_224_/_0.15)] w-full bg-black flex flex-col items-center px-25 pb-30">
+      <header className="xl:bg-green-500 lg:bg-yellow-700 md:bg-orange-400 sm:bg-red-500 shadow-[inset_0px_-140px_180px_60px_rgb(224_224_224_/_0.15)] w-full bg-black flex flex-col items-center md:px-25 md:pb-30 px-10 pb-8">
 
         <nav className={`xl:w-280 w-full h-20 flex justify-between items-center`}>
           <NavButton name="MUDAR" level={500} />
@@ -26,11 +26,11 @@ export default function Home() {
             <DownloadBtn />
           </section>
         </nav>
-        <div className=" lg:w-250 flex lg:flex-row flex-col-reverse items-center justify-evenly mt-25">
-          <Image className="mr-10 lg:ml-15 md:ml-[0%] ml-[60%]" alt="" src={"/Felipe.png"} height={260} width={260}></Image>
-          <div className="h-40 flex flex-col justify-between lg:mt-0 mt-20 w-full">
-            <h1 className={`${S4.className} text-2xl lg:w-150 w-full`}>Prazer, me chamo Felipe Guedes Gonçalves</h1>
-            <p>E sou um desenvolvedor web e mobile</p>
+        <div className=" lg:w-250 flex md:flex-row flex-col-reverse items-center justify-evenly mt-25">
+          <Image className="flex-shrink-0 object-contain mr-10 lg:ml-15 md:ml-[0%] md:w-77 md:h-77 sm:w-60 sm:h-60 w-45 h-45 ml-[60%]" alt="" src={"/Felipe.png"} height={260} width={260}></Image>
+          <div className="md:h-40 flex flex-col justify-between w-full lg:mb-10 md:mb-20">
+            <h1 className={`${S4.className} md:text-2xl lg:w-150 w-full`}>Prazer, me chamo Felipe Guedes Gonçalves</h1>
+            <p className="md:text-base text-sm lg:my-0 my-5">E sou um desenvolvedor web e mobile</p>
             <div className="w-30 flex justify-between">
               <button> <Image alt="" src={"FigmaIcon.svg"} width={30} height={10} /> </button>
               <button> <Image alt="" src={"GithubIcon.svg"} width={30} height={10} /> </button>
@@ -40,9 +40,9 @@ export default function Home() {
         </div>
       </header>
 
-      <section className="w-full bg-neutral-800 p-25 flex flex-col items-center justify-center">
-        <h2 className={`${S4.className} text-xl xl:w-280 w-full`}>Sobre Mim</h2>
-        <div className="mt-10 flex lg:flex-row flex-col justify-between text-justify xl:w-280">
+      <section className="w-full bg-neutral-800 md:p-25 sm:p-17 sm:pt-23 p-10 flex flex-col items-center justify-center">
+        <h2 className={`${S4.className} md:text-xl xl:w-280 w-full`}>Sobre Mim</h2>
+        <div className="md:text-base text-sm mt-10 flex lg:flex-row flex-col justify-between text-justify xl:w-280">
 
           <p className={`lg:w-[60%]`}>Olá, meu nome é Felipe, e sou apaixonado por design e pela criação de experiências únicas. Desde que ingressei no mundo da tecnologia, fico cada vez mais fascinado com o impacto positivo que podemos gerar por meio de soluções inovadoras. Acredito que a tecnologia deve sempre estar a serviço da melhoria contínua da humanidade.
             <br />
@@ -55,14 +55,14 @@ export default function Home() {
 
           <div className="lg:w-[35%] h-full">
             <div>
-              <h4 className={`${S4.className} text-sm w-full lg:m-0 mt-30`}>Minha Formação Acadêmica</h4>
+              <h4 className={`${S4.className}  w-full lg:m-0 mt-30 text-left`}>Minha Formação Acadêmica</h4>
               <p className={`text-yellow-400 mt-5 font-bold`}>Tecnólogo em Análise e desenvolvimento de sistemas</p>
               <p className={`mt-3`}>FIAP - São Paulo - SP, Brasil | fev 2023 - nov 2024</p>
             </div>
             <div>
-              <h4 className={`${S4.className} mt-10 text-sm w-full`}>Línguas faladas</h4>
+              <h4 className={`${S4.className} mt-10  w-full`}>Línguas faladas</h4>
               <p className={`mt-5`}><span className="text-yellow-400 font-bold">Português:</span> Língua Nativa</p>
-              <p className={`mt-3`}><span className="text-yellow-400 font-bold">Inglês:</span> Fluente (nível C1) - Alta proficiência em leitura e escuta, aprimorando fala e escrita</p>
+              <p className={`mt-3`}><span className="text-yellow-400 font-bold">Inglês:</span> Fluente (nível C1)</p>
             </div>
 
           </div>
@@ -72,7 +72,7 @@ export default function Home() {
 
       <section className="w-full h-130 bg-neutral-800 p-25 flex flex-col items-center justify-center">
         <div className="xl:w-280 w-full">
-          <h2 className={`${S4.className} text-xl xl:w-280`}>Contatos</h2>
+          <h2 className={`${S4.className} md:text-xl xl:w-280`}>Contatos</h2>
           <div className="mt-10 flex flex-col justify-between text-justify xl:w-280">
             <p className=""><span className="text-yellow-400 font-bold">Email:</span> <a href="https://mail.google.com/mail/?view=cm&fs=1&to=felipegoncalvesctt@gmail.com&su=Contato%20via%20Portifólio">felipegoncalvesctt@gmail.com</a></p>
             <p className="mt-4"><span className="text-yellow-400 font-bold">LinkedIn:</span> <a href="https://www.linkedin.com/in/felipeguedesgoncalves/">https://www.linkedin.com/in/felipeguedesgoncalves/</a></p>
