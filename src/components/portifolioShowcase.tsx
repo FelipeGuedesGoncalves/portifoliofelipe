5fbbfc4605541cf40257b1245a05ee99cb8b5d93
FelipeGuedesGoncalves <felipeguedes6559@gmail.com>
2025-11-16 21:31:56 -0300

    de volta a estaca zero no carrosel

--- src/components/portifolioShowcase.tsx
+++ src/components/portifolioShowcase.tsx
@@ -11,19 +11,19 @@ export default function PortShow() {
     const [wrapperW, setWrapperW] = useState<string | null>(null);
     const [textStyle, setTextStyle] = useState<string | null>(null);
     const [selectedText, setSelectedText] = useState<string | null>(null);
-    const [showImage, setShowImage] = useState(true);
+    const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
 
     function handleClick(idx: number) {
         if (selected === idx) {
             // Deselecionando
             setDisabled(true);
-            setSumir(null); // remove hidden imediatamente para animar a volta
+            setSumir(null);
 
-            setTextStyle("lg:w-1/2 lg:h-170 h-200 lg:ml-10 lg:mt-0 lg:p-0 p-6");
+            setTextStyle("lg:w-full lg:h-170 h-200 lg:ml-10 lg:mt-0 lg:p-0 p-6");
 
             setTimeout(() => {
-                setTextStyle("lg:w-1/2 lg:mt-0 lg:p-0 px-6 h-0");
+                setTextStyle("lg:w-full lg:mt-0 lg:p-0 px-6 h-0");
             }, 200)
 
             setTimeout(() => {
@@ -32,29 +32,26 @@ export default function PortShow() {
                 setWrapperW(null);
                 setTextStyle("");
                 setSelectedText(null)
-                setShowImage(true);
-            }, 850); // tempo da animação de volta
+            }, 850);
         } else {
             // Selecionando
             setSelected(idx);
             setDisabled(true);
 
             setTimeout(() => {
-                setSumir("hidden"); // esconde após a animação de saída
-                setWrapperW("lg:w-[50%] lg:p-0 p-6 ");
+                setSumir("hidden");
+                setWrapperW("lg:flex-shrink-0 lg:w-150 lg:p-0 p-6");
                 setDisabled(false);
-                setTextStyle("lg:w-1/2 h-0 lg:mt-0 lg:p-0 p-6");
-            }, 600); // tempo da animação de saída
+                setTextStyle("lg:w-full h-0 lg:mt-0 lg:p-0 p-6");
+            }, 600);
 
             setTimeout(() => {
-                setTextStyle("lg:w-1/2 lg:h-170 h-200 lg:ml-10 lg:mt-0 lg:p-0 p-6");
+                setTextStyle("lg:w-full lg:h-170 h-200 lg:ml-10 lg:mt-0 lg:p-0 p-6");
                 setSelectedText(projetos[idx].desc);
             }, 800)
 
             setTimeout(() => {
-                setTextStyle("lg:w-1/2 lg:h-170 lg:ml-10 lg:mt-0 lg:p-0 p-6");
-                setSelectedText(projetos[idx].desc);
-                setShowImage(false);
+                setTextStyle("lg:w-full lg:h-170 lg:ml-10 lg:mt-0 lg:p-0 p-6");
             }, 1000)
         }
     }
@@ -64,106 +61,97 @@ export default function PortShow() {
             capa: "/InsightWise/InsightWiseV.png",
             fundo: "bg-[rgb(135,36,255)] [box-shadow:inset_0_-30_60px_20px_rgb(119,0,255)]",
             delay: "delay-100",
-            desc: "descrição de INSIGHTWISE Lorem ipsum dolor sit, amet consectetur adipisicaaaaaaaaaaaaaaa a aaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! ",
-            imagens: Array.from({ length: 3 }, (_, i) => `/InsightWise/${i + 1}.png`)
-
+            desc: "descrição de INSIGHTWISE Lorem ipsum dolor sit...",
+            imagens: Array.from({ length: 4 }, (_, i) => `/InsightWise/${i + 1}.png`)
         },
         {
             capa: "/PortoBike/PortoBikeV.png",
             fundo: "bg-[rgb(255,255,255)]",
             delay: "delay-200",
-            desc: "descrição de PORTOBIKE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! ",
+            desc: "descrição de PORTOBIKE Lorem ipsum dolor sit...",
             imagens: Array.from({ length: 3 }, (_, i) => `/PortoBike/${i + 1}.png`)
         },
         {
             capa: "/HeatWise/HeatWiseV.png",
             fundo: "bg-[rgb(255,168,0)] [box-shadow:inset_0_-30_60px_20px_rgb(255,136,0)]",
             delay: "delay-300",
-            desc: "descrição de HEATWISE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! ",
-                        imagens: Array.from({ length: 3 }, (_, i) => `/InsightWise/${i + 1}.png`)
+            desc: "descrição de HEATWISE Lorem ipsum dolor sit...",
+            imagens: Array.from({ length: 3 }, (_, i) => `/InsightWise/${i + 1}.png`)
         },
         {
             capa: "/Hunzer/HunzerV.png",
             fundo: "bg-[rgb(147,215,3)] [box-shadow:inset_0_-30_60px_20px_rgb(100,199,43)]",
             delay: "delay-400",
-            desc: "descrição de HUNZER Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! ",
-                        imagens: Array.from({ length: 3 }, (_, i) => `/InsightWise/${i + 1}.png`)
+            desc: "descrição de HUNZER Lorem ipsum dolor sit...",
+            imagens: Array.from({ length: 3 }, (_, i) => `/InsightWise/${i + 1}.png`)
         },
         {
             capa: "/BlueHope/BlueHopeV.png",
-            fundo: "bg-[rgb(27,87,245)] [box-shadow:inset_0_-30_60px_20px_rgb(21,70,196)]",
+            fundo: "bg-[rgb(27,87,245)] [box-shadow:inset_0_-30_60px_20px_rgb(0,42,228)]",
             delay: "delay-500",
-            desc: "descrição de BLUEHOPE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! ",
-                        imagens: Array.from({ length: 3 }, (_, i) => `/InsightWise/${i + 1}.png`)
+            desc: "descrição de BLUEHOPE Lorem ipsum dolor sit...",
+            imagens: Array.from({ length: 3 }, (_, i) => `/InsightWise/${i + 1}.png`)
         },
         {
             capa: "/GridHub/GridHubV.png",
             fundo: "bg-[rgb(0,135,255)] [box-shadow:inset_0_-30_60px_20px_rgb(0,102,255)]",
             delay: "delay-600",
-            desc: "descrição de GRIDHUB Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! ",
-                        imagens: Array.from({ length: 3 }, (_, i) => `/InsightWise/${i + 1}.png`)
+            desc: "descrição de GRIDHUB Lorem ipsum dolor sit...",
+            imagens: Array.from({ length: 3 }, (_, i) => `/InsightWise/${i + 1}.png`)
         },
         {
             capa: "/BabyCare/BabyCareV.png",
             fundo: "bg-[rgb(73,106,247)] [box-shadow:inset_0_-30_60px_20px_rgb(99,66,245)]",
             delay: "delay-700",
-            desc: "descrição de BABYCARE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! ",
-                       imagens: Array.from({ length: 3 }, (_, i) => `/InsightWise/${i + 1}.png`)
+            desc: "descrição de BABYCARE Lorem ipsumLorem ipsum dolor sitLorem ipsum dolor sitLorem ipsum dolor sitLorem ipsum dolor sitLorem ipsum dolor sitLorem ipsum doloLorem ipsum dolor sitLorem ipsum dolor sitLorem ipsum dolor sitLorem ipsum dolor sitLorem ipsum dolor sitLorem ipsum dolor sitLorem ipsum dolor sitLorem ipsum dolor sitLorem ipsum dolor sitLorem ipsum dolor sitLorem ipsum dolor sitLorem ipsum dolor sitLorem ipsum dolor sitLorem ipsum dolor sitLorem ipsum dolor sitLorem ipsum dolor sitLorem ipsum dolor sitLorem ipsum dolor sitLorem ipsum dolor sitLorem ipsum dolor sitLorem ipsum dolor sitLorem ipsum dolor sitr sitLorem ipsum dolor sitLorem ipsum dolor sit dolor sit...",
+            imagens: Array.from({ length: 4 }, (_, i) => `/InsightWise/${i + 1}.png`)
         },
         {
             capa: "/Geeko/GeekoV.png",
             fundo: "bg-[rgb(255,31,53)] [box-shadow:inset_0_-30_60px_20px_rgb(255,0,98)]",
             delay: "delay-800",
-            desc: "descrição de GEEKO Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam veniam explicabo illum et ut animi sunt, vel odit iste. Quo error modi asperiores commodi aliquid magni repellat dolor assumenda consequuntur! ",
-                       imagens: Array.from({ length: 3 }, (_, i) => `/InsightWise/${i + 1}.png`)
+            desc: "descrição de GEEKO Lorem ipsum dolor sit...",
+            imagens: Array.from({ length: 3 }, (_, i) => `/InsightWise/${i + 1}.png`)
         }
     ];
 
-const ProjectCards = projetos.map((projeto, index) => {
+    const ProjectCards = projetos.map((projeto, index) => {
+        return (
+            <button
+                disabled={disabled}
+                key={index}
+                onClick={() => handleClick(index)}
+                className={`overflow-hidden !rounded-xl ${projeto.fundo} ${projeto.delay} flex justify-center items-center transition-all duration-600
+                    ${selected === null
+                        ? `lg:h-70 lg:w-60 lg:p-14 lg:m-5 card p-8 w-43 m-2 h-50`
+                        : selected === index
+                            ? "delay-600 lg:h-170 lg:p-0 p-10 h-100 w-full"
+                            : `scale-0 opacity-0 ${sumir} w-0 h-0`
+                    }`}
+            >
+                    <Image alt="" src={projeto.capa} width={360} height={200} />
 
-    const Carousel = projeto.imagens.map((img, i) => (
-        <Image
-            key={i}
-            alt=""
-            src={img}
-            width={360}
-            height={200}
-        />
-    ));
-
-    return (
-        <button
-            disabled={disabled}
-            key={index}
-            onClick={() => handleClick(index)}
-            className={`!rounded-xl ${projeto.fundo} ${projeto.delay} flex justify-center items-center transition-all duration-600 p
-                ${selected === null
-                    ? `lg:h-70 lg:w-60 lg:p-14 lg:m-5 card p-8 w-43 m-2 h-50`
-                    : selected === index
-                        ? "delay-600 lg:h-170 lg:p-0 p-10 h-100 w-full"
-                        : `scale-0 opacity-0 ${sumir} w-0 h-0`
-                }`}
-        >
-            {selected === index && showImage === false
-                ? Carousel
-                : <Image alt="" src={projeto.capa} width={360} height={200} />
-            }
-        </button>
-    );
-});
-
+            </button>
+        );
+    });
 
     const ProjectDesc = selected !== null ? (
         <p className={`overflow-hidden transition-all duration-600 text-justify ${textStyle}`}>{selectedText}</p>
     ) : null;
 
+    
+
     return (
-        <div className=" w-full flex lg:flex-row flex-col justify-between lg:mt-10 xl:w-300">
-            <div className={`${wrapperW} `}>
+        <div className="w-full flex lg:flex-row flex-col justify-between lg:mt-10 xl:w-300">
+            
+            
+            
+            <div className={`${wrapperW}`}>
                 <div className="w-full flex flex-wrap justify-center">
                     {ProjectCards}
                 </div>
             </div>
+
             {ProjectDesc}
         </div>
     );
