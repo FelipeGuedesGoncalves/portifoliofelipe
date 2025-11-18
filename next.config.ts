556b9a45b44212925b0bb84b6b9ba2014ecad8a6
FelipeGuedesGoncalves <felipeguedes6559@gmail.com>
2025-11-18 07:10:01 -0300

    tentando publicar no github

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",                     // para gerar site estático
   reactCompiler: true,
+  images: { unoptimized: true },        // se usar next/image
+  basePath: process.env.NODE_ENV === "production" ? "/SEU-REPO" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/SEU-REPO/" : "",
+  trailingSlash: true,                  // ajuda no GitHub Pages
 };
 
 export default nextConfig;
